227b41c35e4b8c9def0140b119b9d755089ef165
tihov-eugeny <bes-prokaznik@mail.ru>
2026-04-18 12:34:29 +0300

    add sum

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 void main() {
-    IO.println("Hello and welcome!");
+    int sum = 0;
     for (int i = 1; i <= 5; i++) {
-        IO.println("i = " + i);
+        sum += i;
     }
+    System.out.println("Сумма : " + sum);
 }
